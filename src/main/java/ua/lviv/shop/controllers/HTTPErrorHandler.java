3633a9e865ca185522d861f29eed8911899ae6b1
nastyanahrebna <bybycik@gmail.com>
2016-09-14 15:46:18 +0300

    error pages

--- src/main/java/ua/lviv/shop/controllers/HTTPErrorHandler.java
+++ src/main/java/ua/lviv/shop/controllers/HTTPErrorHandler.java
@@ -1,7 +1,28 @@
 package ua.lviv.shop.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by wild_bo on 14.09.16.
  */
+
+@Controller
 public class HTTPErrorHandler {
+
+    @RequestMapping(value = "/400")
+    public String error400(){
+        return "views-error-400";
+    }
+
+    @RequestMapping(value = "/404")
+    public String error404(){
+        return "views-error-404";
+    }
+
+    @RequestMapping(value = "/500")
+    public String error500(){
+        return "views-error-500";
+    }
+
 }
--- src/main/webapp/views/error/500.jsp
+++ src/main/webapp/views/error/500.jsp
@@ -6,11 +6,5 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-<head>
-    <title></title>
-</head>
-<body>
 
-</body>
-</html>
+<h1>500</h1>
